b67bc930c7af6c8e52449409c755d2a5de60398d
Lazaro Loureiro JR <lloureiro2887@eagle.fgcu.edu>
2026-04-21 13:10:23 -0400

    Delete js directory

--- js/script.js
+++
@@ -1,7 +0,0 @@
-import { FruitopiaGirlsApp } from './modules/fruitopia-girls-app.js';
-
-// Bootstrap the page once the DOM is ready.
-document.addEventListener('DOMContentLoaded', () => {
-    const app = new FruitopiaGirlsApp(document);
-    app.init();
-});
